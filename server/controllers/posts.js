2fdd1b1134eb495417c3eaf37c79ae13bfdba815
akirayamada-haha <hondamurai99@proton.me>
2022-10-30 21:19:26 -0500

    updated controller functionality
    updated getPosts and added createPost logic to the backend

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,27 @@
 // here we create all the handlers for our routes.  This way we can cleanly seperate the logic into this file and use them as variables in the /routes/posts.js file
 
-export const getPosts = (req, rest) => {
-    rest.send('THIS WORKS!');
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
 }
+
+export const createPost =  async (req, res) => {
+    const body = req.body;
+
+    const newPost = new PostMessage(post);
+
+    try {
+        await newPost.save();
+        // codes at https://www.restapitutorial.com/httpstatuscodes.html
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });        
+    }
+};
